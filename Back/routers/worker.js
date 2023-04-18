676019d7aac5d9fe32e71d79d6e806aeaadb72aa
nehoray <nehoraybendor@gmail.com>
2023-04-18 13:48:11 +0300

    save routers delete and patch and srech one person

--- Back/routers/worker.js
+++ Back/routers/worker.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const { getAllWorkers, addWorker } = require("../services/worker.service");
+const { getAllWorkers, addWorker, editWorker } = require("../services/worker.service");
 const Joi = require("joi");
 const router = express.Router();
 
-router.get("/",async(req,res)=>{
-  res.json({msg:"Router work"});
-})
-
-router.get('/getAll', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const response = await getAllWorkers()
     res.status(200).json({ response })
@@ -16,6 +13,14 @@ router.get('/getAll', async (req, res) => {
   }
 })
 
+router.get('/:id', async (req, res) => {
+  try {
+    const response = await getAllWorkers(req.params.id)
+    res.status(200).json({ response })
+  } catch (error) {
+    res.json({ error })
+  }
+})
 
 router.post('/', async (req, res) => {
   try {
@@ -30,10 +35,33 @@ router.post('/', async (req, res) => {
   }
 })
 
-//* Update 
+
+router.patch('/:id', async (req, res) => {
+  try {
+    const response = await editWorker(req.params.id, req.body)
+    res.status(200).json({response})
+  } catch (error) {
+    if (error.details) {
+      res.status(400).json({ error: error.details })
+    } else {
+      res.status(500).json({ error })
+    }
+  }
+})
 
 
-//* Delete 
+router.delete('/:id', async (req ,res) => {
+  try {
+    const response = await UserModel.deleteOne({_id:req.params.id});
+    res.json(response);
+  } catch (error) {
+    if (error.details) {
+      res.status(400).json({ error: error.details })
+    } else {
+      res.status(500).json({ error })
+    }
+  }
+})
 
 
 module.exports = router;
