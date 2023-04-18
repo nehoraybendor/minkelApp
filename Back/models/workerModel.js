d1e1dae5ff67ba5e1fef6a55ccc21de0fd445b08
nehoray <nehoraybendor@gmail.com>
2023-04-18 13:46:55 +0300

    routers in worker delete ,patch,and search one worker

--- Back/models/workerModel.js
+++ Back/models/workerModel.js
@@ -22,16 +22,34 @@ const workerSchema = new mongoose.Schema({
 exports.workerModel = mongoose.model("workers", workerSchema);
 
 
-exports.validateWorker = (reqBody) => {
-    let joiSchema = Joi.object({
-        name: Joi.string().alphanum().min(2).max(30).required(),
-        email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-        age: Joi.number().min(16).max(120).required(),
-        gender: Joi.string().min(2).max(15).required(),
-        phone_number: Joi.string().required(),
-        address: Joi.string().min(3).max(150).required(),
-        salary: Joi.number().min(29.12).max(500),
-        img_profil:Joi.string().max(200).optional()
-    });
-    return joiSchema.validate(reqBody);
+const createWorkerSchema = Joi.object().keys({
+    name: Joi.string().alphanum().min(2).max(30).required(),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    age: Joi.number().min(16).max(120).required(),
+    gender: Joi.string().min(2).max(15).required(),
+    phone_number: Joi.string().required(),
+    address: Joi.string().min(3).max(150).required(),
+    salary: Joi.number().min(29.12).max(500),
+});
+
+const updateWorkerSchema = Joi.object().keys({
+    name: Joi.string().alphanum().min(2).max(30),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    age: Joi.number().min(16).max(120),
+    gender: Joi.string().min(2).max(15),
+    phone_number: Joi.string(),
+    address: Joi.string().min(3).max(150),
+    salary: Joi.number().min(29.12).max(500),
+}).min(1);
+
+// const updateWorkerSchema = createWorkerSchema.fork(Object.keys(createWorkerSchema.describe().keys),key=>{
+//     Joi.when(Joi.required(), { then: Joi.optional()})
+// })
+
+exports.validateWorker = (reqBody) => { 
+    return createWorkerSchema.validate(reqBody);
+}
+
+exports.validateUpdateWorker = (reqBody) => {
+    return updateWorkerSchema.validate(reqBody);
 }
