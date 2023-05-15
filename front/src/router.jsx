ce33f26891707af1870fc4bda547d5186c956c45
Shmouel kessous <chmouel2690@gmail.com>
2023-05-15 19:16:55 +0300

    Shmouel

--- front/src/router.jsx
+++ front/src/router.jsx
@@ -2,12 +2,16 @@ import {Route, createBrowserRouter, createRoutesFromElements} from "react-router
 import Layout from "./components/layout/layout"
 import SideBar from "./components/sideBar/sideBar"
 import Home from "./components/pages/home"
+import LoggedUser from "./guards/loggedUser"
+import Login from "./components/pages/Login"
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Layout/>} >
+            
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/home" element={<LoggedUser><Home/></LoggedUser>}/>
             <Route path="/alog" element={<SideBar/>}>
-                <Route path="/alog/home" element={<Home/>}/>
             </Route>
             <Route path="*" element={<h1>not found 404</h1>}/>
         </Route>
