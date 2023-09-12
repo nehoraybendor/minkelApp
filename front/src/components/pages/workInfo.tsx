65f5fa5c9d5406980550c3890ff4191587754296
nehoray <nehoraybendor@gmail.com>
2023-09-12 20:11:51 +0300

    worker fixed

--- front/src/components/pages/workInfo.tsx
+++ front/src/components/pages/workInfo.tsx
@@ -10,7 +10,13 @@ function WorkInfo({ profile }) {
                     </div>
                 </div>
                 <h3 className="font-bold text-lg text-center mt-4">Name: {profile.name}</h3>
-                <p className="px-[40px] py-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga iusto possimus fugit dicta suscipit consequuntur fugiat in velit placeat, totam culpa quod perferendis molestiae animi nisi? Blanditiis atque fugiat sunt.</p>
+                
+                      <p>Email: {profile.email}</p>
+                      <p>Age: {profile.age}</p>
+                      <p>Gender: {profile.gender}</p>
+                      <p>Phone Number: {profile.phone_number}</p>
+                      <p>Address: {profile.address}</p>
+                      <p>Salary: {profile.salary}</p>
                 <div className='flex justify-center'>
                     <button className="btn btn-outline btn-success mx-5">Success</button>
                     <button className="btn btn-outline btn-warning">Warning</button>
--- front/src/components/pages/workers.tsx
+++ front/src/components/pages/workers.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { BASE_URL } from '../../constant'
 import { useFindAllWorkersQuery } from '../../redux/API/workers.api';
 import { HiOutlinePlusCircle } from "react-icons/hi";
 import AddWorker from '../compos/addWorker';
@@ -6,10 +7,10 @@ import AddWorker from '../compos/addWorker';
 const Workers = () => {
 
   const { isError, isLoading, isSuccess, error, data: workers } = useFindAllWorkersQuery(0)
-
-
+  const [sales, setSales] = useState([])
+  const [loadning, setLoading] = useState(false)
+  const url = 'http://' + BASE_URL + "/dealClient/list";
   if (isError && error) console.log(error);
-
 
 
 
@@ -17,40 +18,40 @@ const Workers = () => {
 
   return (
     <div className='mx-auto'>
-      <button onClick={() => window.addWorker.showModal()}>
+      <button onClick={() => (window as any ).btnadworker.showModal()}>
         <HiOutlinePlusCircle className='w-14 h-14 text-gray-500 dark:text-gray-400 mt-12 -mb-20 mr-[400px]' />
       </button>
       <h2 className='text-center text-[50px] mt-2'>WORKER</h2>
       <div className="flex flex-row flex-wrap justify-center mx-[32px]">
-        {isLoading ?
-
+        {loadning ? (
           <span className="place-self-center loading loading-spinner loading-lg"></span>
-          : workers && isSuccess &&
-          workers?.map((worker, i) => (
+        ) : (
+          sales?.map((item, i) => (
             <div className="flex flex-row flex-wrap justify-center mx-[32px]">
-
-              <div className="card card-side bg-base-100 shadow-xl h-[200px] w-[300px] m-[20px]" key={i}>
-                <figure>
-                  <img className="w-[200px]" src={worker.profil_url} alt="Worker" />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">{worker.name}</h2>
-                  <p>Email: {worker.email}</p>
-                  <p>Age: {worker.age}</p>
-                  <p>Gender: {worker.gender}</p>
-                  <p>Phone Number: {worker.phone_number}</p>
-                  <p>Address: {worker.address}</p>
-                  <p>Salary: {worker.salary}</p>
-                </div>
-              </div>
-
+              {isLoading ? (
+                <span className="place-self-center loading loading-spinner loading-lg"></span>
+              ) : isSuccess && workers && (
+                workers.map((worker, i) => (
+                  <div className="card card-side bg-base-100 shadow-xl h-[200px] w-[300px] m-[20px]" key={i}>
+                    <figure>
+                      <img className="w-[200px]" src={worker.profil_url} alt="Worker" />
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title">{worker.name}</h2>
+                      <p>Name: {worker.name}</p>
+                      <p>Age: {worker.age}</p>
+                      <p>Gender: {worker.gender}</p>
+                      <button className="btn btn-info">Info</button>
+                    </div>
+                  </div>
+                ))
+              )}
             </div>
-          )
-          )}
-
+          ))
+        )}
       </div>
 
-      <AddWorker />
+<AddWorker/>
 
     </div>
 
